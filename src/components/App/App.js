dd6acc6414f5e59f8c36558092e417b8afa88675
Ronan Levesque <ronan.levesque@gmail.com>
2017-04-12 15:01:38 +0200

    Updated App component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,9 +3,11 @@ import styles from './App.css';
 import logo from '../../img/logo-react.png';
 
 const App = () => (
-  <div>
-    <img className={styles.logo} src={logo} alt="React logo" />
-    <h1 className={styles.title}>Hello world!</h1>
+  <div className={styles.wrapper}>
+    <div className={styles.content}>
+      <img className={styles.logo} src={logo} alt="React logo" />
+      <h1 className={styles.title}>Hello world!</h1>
+    </div>
   </div>
 );
 
